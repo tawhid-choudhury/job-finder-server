9c6977156bd1cab126f5695cbf3c00d85bddd3ef
pktatari <tawhidalmuhaimin@gmail.com>
2023-11-08 06:32:57 +0600

    delete job added

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/alljobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
