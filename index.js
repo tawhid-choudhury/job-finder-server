eabecf42c5426e3682455686e8ee5c6d014099ae
pktatari <tawhidalmuhaimin@gmail.com>
2023-11-09 18:31:48 +0600

    last commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
+// tawhid
 app.use(
   cors({
     // origin: ["http://localhost:5173", "http://localhost:5174"],
